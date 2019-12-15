ff557a8e7fcf9294d0128bc0e056c6a2f3202f46
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2019-12-15 09:37:58 -0500

    Update diddlySquat.c

--- Programs/diddlySquat.c
+++ Programs/diddlySquat.c
@@ -1,2 +1,10 @@
-    /*
-   int main (void) { return 0; }
+int           /* The first  part tells the return type.       */
+main          /* The second part tells the name.              */
+(void)        /* The third  part tells the input.             */
+{ return 0; } /* The fourth part tells what to do.            */
+
+/*      The fourth part
+ *         -    begins with an open brace, "{",
+ *         -    has one or more statements; here, "return 0;"
+ *         -    ends with a close brace. "}".
+ */
