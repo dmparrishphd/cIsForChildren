0a871a23bc187544518d9eaecccfd26267dacd77
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-04-13 21:16:15 -0400

    Update diddlySquat.c

--- Parts/Programs/diddlySquat.c
+++ Parts/Programs/diddlySquat.c
@@ -1,10 +1,17 @@
-int           /* The first  part tells the return type.       */
-main          /* The second part tells the name.              */
-(void)        /* The third  part tells the input.             */
-{ return 0; } /* The fourth part tells what to do.            */
-
-/*      The fourth part
- *         -    begins with an open brace, "{",
- *         -    has one or more statements; here, "return 0;"
- *         -    ends with a close brace. "}".
- */
+/**************************************************************************
+***                                                                      **
+***     This program begins with comments.      Comments are symbols     **
+***     that your computer will ignore. A comment begins with "slash     **
+***     star",     /*     and     ends     with     "star    slash",     **
+***     */                                                               /*
+***     ************************************************************     **
+***                                                                      **
+**/     int         /* The first  part tells the return type (int).      **
+**/     main        /* The second part tells the name ("main").          **
+**/     (void)      /* The third  part tells the input (nothing).        **
+**/                 /* The fourth part tells what to do. It...           **
+**/     {           /* begins with an open brace, "{",...                **
+**/     return 0;   /* has one or more statements, and ...               **
+**/     }           /* ends with a close brace. "}".                     **
+***                                                                      **
+***     ************************************************************     */
