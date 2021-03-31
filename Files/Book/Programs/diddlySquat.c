bdac1fc997c37d50df3b59ee12a236b3eacbbed3
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-03-31 14:50:53 -0400

    Update diddlySquat.c

--- Files/Book/Programs/diddlySquat.c
+++ Files/Book/Programs/diddlySquat.c
@@ -1,17 +1,12 @@
-/**************************************************************************
-***                                                                      **
-***     This program begins with comments.      Comments are symbols     **
-***     that your computer will ignore. A comment begins with "slash     **
-***     star",     /*     and     ends     with     "star    slash",     **
-***     */                                                               /*
-***     ************************************************************     **
-***                                                                      **
-**/     int         /* The first  part tells the return type (int).      **
-**/     main        /* The second part tells the name ("main").          **
-**/     (void)      /* The third  part tells the input (nothing).        **
-**/                 /* The fourth part tells what to do. It...           **
-**/     {           /* begins with an open brace, "{",...                **
-**/     return 0;   /* has one or more statements, and ...               **
-**/     }           /* ends with a close brace. "}".                     **
-***                                                                      **
-***     ************************************************************     */
+/*
+This program begins with comments.
+Comments are text that your computer will ignore.
+A comment begins with "slash star" and ends with "star slash,"
+*/
+int           /* The first  part tells the return type (int). */
+main          /* The second part tells the name ("main").     */
+(void)        /* The third  part tells the input (nothing).   */
+              /* The fourth part tells what to do. Part four  */
+{             /* begins with an open brace, "{",              */
+    return 0; /* has one or more statements, and              */
+}             /* ends with a close brace. "}".                */
