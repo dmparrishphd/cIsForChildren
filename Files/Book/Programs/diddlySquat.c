2b09c2a8733b0d0b1f6cb593625847c46a947080
D. Michael Parrish <39537744+dmparrishphd@users.noreply.github.com>
2025-01-13 13:43:12 -0500

    Incorporate end-of-line comments

--- Files/Book/Programs/diddlySquat.c
+++ Files/Book/Programs/diddlySquat.c
@@ -1,8 +1,29 @@
-/*
-This program begins with comments.
-Comments are text that your computer will ignore.
-A comment begins with "slash star" and ends with "star slash,"
-*/
+/*  This program begins with comments.
+ *  There are two kinds of comments:
+ *  inline or multiline comments, like
+ *  the one you are reading now, and
+ *  end-of-line comments.
+ *    Comments are text that the ___
+ *  omits in phase ___. Nothing in
+ *  comments affects the program.
+ *    An inline or multiline comment
+ *  begins with "slash star."
+ *    It is common to begin each
+ *  additional comment line with a star;
+ *  this is a convention, not a
+ *  requirement.
+ *    An inline or multiline comment ends
+ *  with "star slash."
+ */
+//  This is an end-of-line comment.
+//  End-of-line comments begin with
+//  "slash-slash" and end with an end-
+//  of-line marker.
+//
+//  Except for the comments and extra
+//  whitespace, this is the simplest
+//  possible program. It is comprised
+//  by a single function.
 int           /* The first  part tells the return type (int). */
 main          /* The second part tells the name ("main").     */
 (void)        /* The third  part tells the input (nothing).   */
